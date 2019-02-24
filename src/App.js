6e12cbd063e66604490051d097e3e34b67c79a45
Mielyn Acosta <mielynacosta@pursuit.org>
2019-02-24 17:05:51 -0500

    created initial video display

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import React, { Component } from 'react';
+import VideoPlayer from './components/VideoPlayer/VideoPlayer';
+
+
+
 
 class App extends Component {
   render() {
     return (
+      <React.Fragment>
       <div className="App">
         <p>We are the best team! don't @ me</p>
       </div>
+      <VideoPlayer />    
+      </React.Fragment>
     );
   }
 }
