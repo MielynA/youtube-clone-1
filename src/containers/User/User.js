3e6018401bac0b42275aa87af8d871924477d181
Erika Perez <erikaperezvalle@pursuit.org>
2019-02-24 22:46:19 -0500

    added alert, alert has custome message for either invalid input or user already exists

--- src/containers/User/User.js
+++ src/containers/User/User.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { UserList } from '../../components/Lists/UserList';
 import { AddNew } from '../../components/AddNew/AddNew';
-import { Row, Col } from 'reactstrap';
+import { Row, Col, Alert } from 'reactstrap';
 
 class User extends Component {
     constructor(props) {
@@ -9,11 +9,17 @@ class User extends Component {
         this.state = {
             title: 'User', //title for add form 
             placeholder: "User's name", //placeholder for add form
-            activeUser: 'Taq',
-            users: { 'Mo': { 'feed': { 'sports': [] } }, 'Taq': { 'feed': { 'cats': [] } }, 'Liz': { 'feed': { 'bunnies': [] } } },
+            activeUser: '',
+            users: {},
             input: '',
-            alertOn: false
-        }
+            alertOn: false,
+            error: '' //error message for alert, either invalid entry or existing user
+        };
+        this.onDismiss = this.onDismiss.bind(this);
+    }
+
+    onDismiss() {
+        this.setState({ alertOn: false });//close alert
     }
 
     handleListClick = (index) => {
@@ -23,18 +29,30 @@ class User extends Component {
     }
 
     handleAddClick = (input) => {
-        const upperCase = input[0].toUpperCase();
-        const lowerCase = input.slice(1);
-        const name = upperCase.concat('', lowerCase);
-        const userArr = Object.keys(this.state.users);
-        for (let i = 0; i < userArr.length; i++) {
-            if (name === userArr[i]) {
-                console.log(userArr[i])
-                this.setState({ alertOn: true, input: '' })
-            } else {
+        if (input === '' || input[0] === ' ') {
+            this.setState({ alertOn: true, input: '', error: 'Invalid Input, Please Enter Valid Name.' })
+        }
+        else {
+            const upperCase = input[0].toUpperCase();
+            const lowerCase = input.slice(1);
+            const name = upperCase.concat('', lowerCase);
+            const userArr = Object.keys(this.state.users);
+            if (userArr.length === 0) {
                 const newUser = { [name]: { 'feed': null } }
-                const newUsers = Object.assign(this.state.users, newUser)
-                this.setState({ user: newUsers, input: '' })
+                this.setState({ users: newUser, input: '', alertOn: false })
+            }
+            else {
+                for (let i = 0; i < userArr.length; i++) {
+
+                    if (name === userArr[i]) {
+                        this.setState({ error: 'Name Already Exists, Try A Different Name.', alertOn: true, input: '' })
+                    }
+                    else {
+                        const newUser = { [name]: { 'feed': null } }
+                        const newUsers = Object.assign(this.state.users, newUser)
+                        this.setState({ user: newUsers, input: '', alertOn: false })
+                    }
+                }
             }
         }
     }
@@ -46,9 +64,12 @@ class User extends Component {
 
     render() {
 
-        const { placeholder, title, activeUser, users, input } = this.state;
+        const { placeholder, title, activeUser, users, input, error, alertOn } = this.state;
         const data = { activeUser, users }
         return <>
+            <Alert color="info" isOpen={alertOn} toggle={this.onDismiss}>
+                {error}
+            </Alert>
             <div className="container-fluid">
                 <Row form>
                     <Col>
